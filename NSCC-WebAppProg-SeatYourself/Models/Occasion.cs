8037d14ef4ddf2ad3640c96eab47aa83a564c77e
apb-nscc <the.alchemist.coder@gmail.com>
2025-10-01 11:57:33 -0300

    Models Created

---
+++ NSCC-WebAppProg-SeatYourself/Models/Occasion.cs
@@ -0,0 +1,27 @@
+﻿namespace NSCC_WebAppProg_SeatYourself.Models
+{
+    public class Occasion
+    {
+        //Primary key
+        public int OccasionId { get; set; }
+
+
+        //Attributes
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public string Owner { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+
+
+        //Foreign keys
+        public int VenueId { get; set; }
+        public int CategoryId { get; set; }
+        
+
+        //Navigation properties
+        public Venue? Venue { get; set; }
+        public Category? Category { get; set; }
+    }
+}
